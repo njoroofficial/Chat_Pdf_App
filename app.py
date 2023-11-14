9874e2d77bdba9552bab3bb86638a5236513cd26
njoroofficial <mosesn579@gmail.com>
2023-11-14 23:01:26 +0300

    updated commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_chat import message
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-import openai
+from langchain.chat_models import openai
 from htmlTemplates import css, bot_template, user_template
 from langchain.text_splitter import CharacterTextSplitter # help divide text into chunks
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
